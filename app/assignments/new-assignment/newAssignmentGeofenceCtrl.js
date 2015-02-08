783a89e6b1fb0252aa0d0f9c46094822067d9d76
thequbit <thequbit@gmail.com>
2015-02-08 14:31:48 -0500

    fixed issue with assignment not being published

--- app/assignments/new-assignment/newAssignmentGeofenceCtrl.js
+++ app/assignments/new-assignment/newAssignmentGeofenceCtrl.js
@@ -2,11 +2,8 @@
 var L = L || {};
 
 var map = map || {};
-//var featureGroup = featureGroup || {}
 
-//var drawingBox = false;
-//var topLeftCord = undefined;
-//var bottomRightCord = undefined;
+var mainTileLayer = {};
 
 angular
     .module('Yellr')
@@ -15,7 +12,7 @@ angular
 
         // initialize map
 
-        var main = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        var mainTileLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
              attribution: 'Map data © OpenStreetMap contributors',
              minZoom: 10,
              maxZoom: 16,
@@ -25,15 +22,14 @@ angular
             center: [43.2, -77.6],
             zoom: 10,
             layers: [
-                main
+                mainTileLayer
             ]
         });
 
         map.drawingBox = false;
         map.geoBox = false;
 
-        var button = $('#geo-fence-button');
-        button.on('click', function(e) {
+        $('#geo-fence-button').on('click', function(e) {
             map.enableDrawing = true;
         });
 
@@ -48,11 +44,10 @@ angular
 
         map.on('mousemove', function(e) {
            if ( map.enableDrawing && map.drawingBox ) {
-               if ( map.geoBox != false ) {
+               //if ( map.geoBox == false ) {
                    map.removeLayer(map.geoBox);
-               }
-               var bounds = [map.topLeftCord, e.latlng];
-               map.geoBox = L.rectangle(bounds, {color:"#ff7800", weight:1});
+               //}
+               map.geoBox = L.rectangle([map.topLeftCord, e.latlng], {color:'#ff7800', weight:1});
                map.addLayer(map.geoBox);
            }
         });
@@ -61,18 +56,18 @@ angular
             if ( map.enableDrawing && map.drawingBox ) {
                 map.removeLayer(map.geoBox);
                 var bounds = [map.topLeftCord, e.latlng];
-                map.geoBox = L.rectangle(bounds, {color:"#00FF78", weight:1})
+                map.geoBox = L.rectangle(bounds, {color:'#00FF78', weight:1});
                 map.addLayer(map.geoBox);
 
                 $scope.$parent.assignment.geofence = {
-                    topLeft: [
-                        map.topLeftCord.lat,
-                        map.topLeftCord.lng
-                    ],
-                    bottomRight: [
-                        e.latlng.lat,
-                        e.latlng.lng
-                    ]
+                    topLeft: {
+                        lat: map.topLeftCord.lat,
+                        lng: map.topLeftCord.lng
+                    },
+                    bottomRight: {
+                        lat: e.latlng.lat,
+                        lng: e.latlng.lng
+                    }
                 };
 
                 // Since this event is out of angular's standard event listener
