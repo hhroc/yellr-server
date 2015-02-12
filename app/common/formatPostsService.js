dafd92089c3a28052d1f9ceb7e47fca681d451bf
thequbit <thequbit@gmail.com>
2015-02-12 09:29:56 -0500

    added assignment display for response posts, and added border (gross) to post tiles

--- app/common/formatPostsService.js
+++ app/common/formatPostsService.js
@@ -5,7 +5,9 @@ var moment = moment || {};
 angular
     .module('Yellr')
     .factory('formatPosts', function () {
+        /*
         var _getFirstText = function (post) {
+            
             var i, mediaObject;
             for (i = 0; i < post.media_objects.length; i++) {
                 mediaObject = post.media_objects[i];
@@ -16,9 +18,25 @@ angular
             }
 
             return null;
+            
+        },
+        */
+
+        var _getPostText = function (post) {
+            var retText = '';
+            var mediaObject = post.media_objects[0];
+            if ( mediaObject.media_type_name == 'text' ) {
+                retText = mediaObject.media_text;
+            }
+            else if ( mediaObject.media_type_name == 'image' ) {
+                retText = mediaObject.caption;
+            }
+            return retText;
         },
 
+        /*
         _getFirstImage = function (post) {
+            
             var i, mediaObject;
             for (i = 0; i < post.media_objects.length; i++) {
                 mediaObject = post.media_objects[i];
@@ -31,18 +49,36 @@ angular
                 }
             }
             return null;
+            
+        },
+        */
+
+        _getPostImage = function(post) {
+            var retImage = '';
+            var mediaObject = post.media_objects[0];
+            if ( mediaObject.media_type_name == 'text' ) {
+                retImage = null;
+            }
+            else if ( mediaObject.media_type_name == 'image' ) {
+                retImage = {'background-image': 'url(/media/' +
+                            mediaObject.preview_file_name + ')'};
+            }
+            return retImage;
         },
 
         formatPosts = function (posts) {
             for (var postId in posts) {
-                // TODO: get title and image
                 posts[postId].time = moment(
                         posts[postId].post_datetime,
                         'YYYY-MM-DD HH:mm:ss')
                     .fromNow();
-                posts[postId].description = _getFirstText(
+                //posts[postId].description = _getFirstText(
+                //                                posts[postId]);
+                posts[postId].text = _getPostText(
                                                 posts[postId]);
-                posts[postId].imageUrl = _getFirstImage(
+                //posts[postId].imageUrl = _getFirstImage(
+                //                                posts[postId]);
+                posts[postId].imageUrl = _getPostImage(
                                                 posts[postId]);
             }
 
